ab2f531e100aa586741dc8247700d3ad668a6479
yedigaryansem <semyedigaryan@gmail.com>
2020-10-24 19:54:10 +0400

    added Test Cases

---
+++ Test_Cases/TestCase_3_Chrysler.py
@@ -0,0 +1,11 @@
+from Project_List.Classes.Main import Main
+from Project_List.Atributes.Atributes import ENVIRONMENT
+
+
+main = Main("Chrysler", ENVIRONMENT)
+main.enter_to_environment()
+main.go_to_cars_page()
+main.pick_cars_mark()
+main.cars_count()
+main.liking()
+main.go_to_fav_pub_page()
