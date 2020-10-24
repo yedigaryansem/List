72f0767c0ffd8e5ef5af24539b7e7d932b7a17ba
yedigaryansem <semyedigaryan@gmail.com>
2020-10-24 19:22:49 +0400

    Fixup

--- List.py
+++ List.py
@@ -4,7 +4,7 @@ import time
 from selenium.common.exceptions import NoSuchElementException
 
 
-def autogoing_to_machines_page(driver):
+def autogoing_to_cars_page(driver):
     first_window = driver.find_element_by_xpath('//*[@id="menu"]/div/div[1]')
     action1.move_to_element(first_window).perform()
     second_window = driver.find_element_by_xpath('//*[@id="menu"]/div/div[1]/div/div/span[2]/a')
@@ -26,12 +26,12 @@ def go_last_page(driver):
     return count
 
 
-def count_auto_in_page(driver):
+def count_cars_in_page(driver):
     page_auto_list = driver.find_elements_by_xpath('//*[@id="contentr"]/div[*]/div[1]/a[*]')
     return len(page_auto_list)
 
 
-def find_all_auto_names(driver):
+def find_all_car_names(driver):
     a = driver.find_elements_by_xpath('//*[@id="contentr"]/div[3]/div[1]/a[*]')
     for i in range(len(a)):
         b = a[i].find_element_by_xpath(f'/html/body/div[3]/div[4]/div[2]/div[3]/div[1]/a[{i + 1}]/div[3]') \
@@ -43,17 +43,17 @@ def find_all_auto_names(driver):
         print(b.text)
 
 
-def autocount(driver):
-    first_page_auto_count = count_auto_in_page(driver)
+def car_count(driver):
+    first_page_car_count = count_cars_in_page(driver)
     pages_count = go_last_page(driver)
-    last_page_auto_count = count_auto_in_page(driver)
+    last_page_car_count = count_cars_in_page(driver)
 
-    total_auto_count = first_page_auto_count * pages_count + last_page_auto_count
-    return total_auto_count
-    # print(first_page_auto_count, pages_count, last_page_auto_count)
+    total_cars_count = first_page_car_count * pages_count + last_page_car_count
+    return total_cars_count
+    # print(first_page_car_count, pages_count, last_page_car_count)
 
 
-def auto_mark(driver, mark):
+def car_mark(driver, mark):
     mark_type = driver.find_element_by_xpath('//*[@id="bList"]')
     mark_type.click()
     mark_type.send_keys(mark)
@@ -62,10 +62,10 @@ def auto_mark(driver, mark):
 
 def liking(driver):
     time.sleep(1)
-    random_auto = driver.find_element_by_xpath(
+    random_car = driver.find_element_by_xpath(
         '/html/body/div[3]/div[*]/div[2]/div[@class="dl"]/div[@class="gl"]/a[1]/img')
     action2 = ActionChains(driver)
-    action2.move_to_element(random_auto).perform()
+    action2.move_to_element(random_car).perform()
     action2.reset_actions()
     # time.sleep(2)
     liker = driver.find_element_by_xpath('//*[@id="star"]/div')
@@ -83,17 +83,17 @@ def liking(driver):
         print("")
 
 def login(driver, check):
-    log = driver.find_element_by_xpath('//*[@id="_idyour_email"]')
-    log.click()
-    log.send_keys("loginpa@mail.ru")
-    pas = driver.find_element_by_xpath('//*[@id="_idpassword"]')
-    pas.click()
-    pas.send_keys('123456qweasd')
+    login = driver.find_element_by_xpath('//*[@id="_idyour_email"]')
+    login.click()
+    login.send_keys("loginpa@mail.ru")
+    password = driver.find_element_by_xpath('//*[@id="_idpassword"]')
+    password.click()
+    password.send_keys('123456qweasd')
     # time.sleep(2)
     enter = driver.find_element_by_xpath('//*[@id="loginaction__form_action0"]')
     enter.click()
     if check:
-        auto_mark(driver,mark)
+        car_mark(driver, mark)
         liking(driver)
 
 
@@ -101,8 +101,8 @@ def checker(driver):
     login_icon = driver.find_element_by_xpath('//*[@id="ma"]/img')
     action2 = ActionChains(driver)
     action2.move_to_element(login_icon).perform()
-    favourite_pubs = driver.find_element_by_xpath('//*[@id="ma"]/div/a[5]/div[2]')
-    favourite_pubs.click()
+    favourite_publications = driver.find_element_by_xpath('//*[@id="ma"]/div/a[5]/div[2]')
+    favourite_publications.click()
     # favourite_autos = driver.find_element_by_xpath('//*[@id="contentr"]/div')
     # for i in range(len(favourite_autos)):
     #     favourite_autos[i].find_element_by_xpath('//*[@id="contentr"]/div/a[3]/div[1]/div[1]')
@@ -111,26 +111,25 @@ def checker(driver):
     # return "item is not avelation"
 
 
-mark = str(input("PLease Enter auto mark: "))
+# mark = str(input("PLease Enter auto mark: "))
 driver = webdriver.Chrome()
-driver.maximize_window()
-action1 = ActionChains(driver)
-driver.get("https://www.list.am/")
-autogoing_to_machines_page(driver)
-auto_mark(driver, mark)
-print(mark, autocount(driver),'Штук')
-liking(driver)
-checker(driver)
-# print(checker(driver, check_value))
-# driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+# driver.maximize_window()
+# action1 = ActionChains(driver)
+# driver.get("https://www.list.am/")
+# autogoing_to_cars_page(driver)
+# car_mark(driver, mark)
+# print(mark, car_count(driver), 'Штук')
+# liking(driver)
+# checker(driver)
+# # print(checker(driver, check_value))
+# # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
 
 
 
-
-
-
-
+driver.get("https://www.list.am/category/23/9?bid=5&crc=-1&n=0")
+a = driver.find_element_by_xpath('/html/head/link[3]')
+print(type(a.get_attribute("href")))
 
 
 
